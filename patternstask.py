5132de4404d803281f99dc4a96448ba5f896d3c8
ruthvid <146691698+ruthvid@users.noreply.github.com>
2025-09-03 17:40:46 +0530

    Add files via upload

---
+++ patternstask.py
@@ -0,0 +1,193 @@
+for i in range(1,6):
+    print('*' * i)
+    
+rows = 5    
+for i in range(1,6):
+    print(" "*(rows-i) + "*" * i)
+    
+
+           #OR
+           
+           
+rows = 5
+for i in range(1,rows+1):
+    print(" "*(rows-i),end="")
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+        
+        
+
+
+# A
+# AB
+# ABC
+# ABCD
+# ABCDE
+
+
+rows = 5
+for i in range(1,rows+1):
+    n=65
+    for j in range(1,i+1):
+        print(chr(n),end="")
+        n+=1
+    print()
+    
+    
+# A
+# BC
+# DEF
+# GHIJ
+# KLMNO
+
+rows = 5
+n = 65
+
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print(chr(n),end="")
+        n+=1
+    print()
+
+
+# A
+# BB
+# CCC
+# DDDD
+# EEEEE
+
+rows = 5
+n = 65
+
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print(chr(n),end="")
+    n+=1
+    print()
+
+
+# 1
+# AA
+# 123
+# BBBB
+# 12345
+
+rows = 5
+n = 65
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        if i % 2 ==0:
+            print(chr(n),end="")
+        else:
+            print(j,end="")
+    if i%2 == 0:
+        n+=1
+    print()
+
+
+
+# task 1
+
+rows = 5
+for i in range(1,rows+1):
+    c = 65
+    s = 97
+    if i % 2 ==0:
+        for j in range (1,i+1):
+            print(chr(s),end="")
+            s+=1
+    else:
+        for j in range (1,i+1):
+            print(chr(c),end="")
+            c+=1
+    print()
+    
+# Output:--
+# A
+# ab
+# ABC
+# abcd
+# ABCDE
+
+
+    
+# task 2
+
+rows = 5
+for i in range(1,rows+1):
+    print(" "*(rows-i),end="")
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+    
+# Output:--
+#     1
+#    12
+#   123
+#  1234
+# 12345
+
+
+
+
+# task 3
+
+rows = 5
+for i in range(1,rows+1):
+    print(" "*(rows-i),end="")
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+
+# Output:--
+#     1
+#    22
+#   333
+#  4444
+# 55555
+
+
+# task 4
+
+rows= 5
+for i in range(1,rows+1):
+    print(" "*(rows-i),end="")
+    n = 65
+    for j in range(1,i+1):
+        print(chr(n),end="")
+        n+=1
+    print()
+
+# Output:--
+#     A
+#    AB
+#   ABC
+#  ABCD
+# ABCDE
+
+# task 5
+
+rows = 5
+
+for i in range(1,rows+1):
+    print(" "*(rows-i),end="")
+    n = 65
+    if i % 2 ==0:
+        for j in range(1,i+1):
+            print(j,end="")
+    else:
+        for j in range(1,i+1):
+            print(chr(n),end="")
+            n+=1
+    print()
+
+# Output:--
+#     A
+#    12
+#   ABC
+#  1234
+# ABCDE
+
+
+
